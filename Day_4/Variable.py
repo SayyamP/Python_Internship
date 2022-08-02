fd7bf251d8e5914c22b5392baf17bcc299b0c806
SayyamP <patilsayyam2508@gmail.com>
2022-08-02 17:52:21 +0530

    4th day codes

---
+++ Day_4/Variable.py
@@ -0,0 +1,57 @@
+''' import camelcase
+
+
+c = camelcase.CamelCase()
+
+
+txt = "shiv is handsome boy, he is very smart"
+
+
+print(c.hump(txt))
+
+
+#This method capitalizes the first letter of each word.'''
+
+
+#Variables
+
+
+x = 5
+y = "shiv"
+print(x)
+print(y)
+
+
+x = "Sayyam"
+print(x)
+
+
+#Typecasting
+
+
+x = str(10000000)    # x will be '10000000'
+y = int('500')    # y will be 500
+z = float(50000000000000000000000)
+
+
+print(x)
+print(y)
+print(z)
+
+
+#Checking the dataypes of variables
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#Checking the case sensitivity
+
+
+X = "This_is_a_capital_X" 
+
+
+print("Small x = ", x)
+print("Capital X = ", X)
---
+++ Day_4/modulas.py
@@ -0,0 +1,3 @@
+import os
+import tensorflow
+print("Using different Modulas:-")
